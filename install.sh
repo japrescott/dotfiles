bcf59a2d330b1760e6c6e04c2dd19086664c2784
Jeremy A. Prescott <jap@prescore.ch>
2015-04-21 15:14:36 +0200

    getting mighty dotfiley

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Initialize and update all submodules.
+echo Initializing git submodules.
+git submodule init && git submodule update
+
+# Remove all dotfiles from the home directory if present.
+echo Removing any existing dotfiles from your home directory.
+rm -rf ~/.gitconfig ~/.tmux.conf ~/.tmux_theme ~/.tmux ~/.inputrc
+
+# Initialize symlinks.
+echo Creating symlinks in your home directory that point to this dotfiles repository.
+ln -s "$PWD/.inputrc" ~/.inputrc
+ln -s "$PWD/.gitconfig" ~/.gitconfig
+ln -s "$PWD/.tmux.conf" ~/.tmux.conf
+ln -s "$PWD/.tmux_theme" ~/.tmux_theme
+ln -s "$PWD/.tmux" ~/module/tmux
+
+# Finished.
+echo Dotfiles installation complete.
