e7020437a5feaeb0092ef46a0b79c29e84140960
Jeremy A. Prescott <jeremy@lightly.ai>
2025-05-26 20:56:09 +0200

    restructure install

--- install.sh
+++ install.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
+
+echo ----------------------------
+echo Initializing git.
+
+dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
+if [ "$dist" == "Ubuntu" ]; then
+	apt-get install -a gitk gitx diff-so-fancy
+else
+	brew install git
+fi
+
+
+
 # Initialize and update all submodules.
 echo ----------------------------
 echo Initializing git submodules.
 git submodule init
 git submodule update
 
-# install some dependencies
-echo ----------------------------
-echo Installing some depedendencies
-
-dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
-if [ "$dist" == "Ubuntu" ]; then
-	echo "install ubuntu stuff"
-	apt-get install -a gitk gitx diff-so-fancy
-else
-	echo "installing macosx stuff"
-	brew install tmux
-	brew install diff-so-fancy
-
-	brew install viu # for viewing images in terminal
-	brew install lsix # for viewing images in ls
-	
-fi
 
 # Remove all dotfiles from the home directory if present.
 echo ----------------------------
@@ -45,6 +41,48 @@ echo Adding configs
 # IntelliJ
 mkdir -p ~/.IntelliJIdea15/config/keymaps/
 ln -s "$PWD/conf/intellij.keymap.xml" ~/.IntelliJIdea15/config/keymaps/MyOwn.keymap.xml
+
+
+
+# installing node via nvm
+echo ----------------------------
+echo Installing Node.js via nvm.
+if [ ! -d "$HOME/.nvm" ]; then
+	echo "Installing nvm"
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+fi
+nvm install --lts
+nvm use --lts
+
+# Install global npm packages.
+echo ----------------------------
+echo Installing global npm packages.
+npm install -g \
+	typescript \
+	ts-node \
+	tsx \
+	json-log-viewer
+
+
+
+# install some dependencies
+echo ----------------------------
+echo Installing other depedendencies
+
+
+if [ "$dist" == "Ubuntu" ]; then
+	echo "install ubuntu stuff"
+	apt-get install -a gitk gitx tmux diff-so-fancy
+else
+	echo "installing macosx stuff"
+	brew install tmux
+	brew install diff-so-fancy
+
+	brew install viu # for viewing images in terminal
+	brew install lsix # for viewing images in ls	
+fi
 
 
 # other fun things
